615c7b92d7a1f8fafec8291e0aa5596da7484486
mxnoob <noobmx@gmail.com>
2024-02-13 11:36:22 +0300

    Review models - пока без миграций

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,7 +21,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
+
     'reviews.apps.ReviewsConfig',
 ]
 
@@ -87,7 +89,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,62 @@
+from django.core import validators
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+MAX_COMMENT_LENGTH = 50
+
+User = get_user_model()
+
+
+class AbstractReviewModel(models.Model):
+    author = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+
+    class Meta:
+        abstract = True
+        ordering = ('-pub_date',)
+
+
+class Title(models.Model):
+    pass
+
+
+class Review(AbstractReviewModel):
+    text = models.TextField('Отзыв')
+    score = models.PositiveSmallIntegerField(
+        'Оценка',
+        validators=[
+            validators.MinValueValidator(1),
+            validators.MaxValueValidator(10),
+        ],
+    )
+    title = models.ForeignKey(
+        'Title', verbose_name='Название', on_delete=models.CASCADE
+    )
+
+    class Meta(AbstractReviewModel.Meta):
+        verbose_name = 'Обзор'
+        verbose_name_plural = 'Обзоры'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('title', 'author'), name='unique_title_review'
+            )
+        ]
+        default_related_name = 'reviews'
+
+    def __str__(self):
+        return self.text[:MAX_COMMENT_LENGTH]
+
+
+class Comment(AbstractReviewModel):
+    text = models.TextField('Комментарий')
+    review = models.ForeignKey(
+        Review, verbose_name='Обзор', on_delete=models.CASCADE
+    )
+
+    class Meta(AbstractReviewModel.Meta):
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        default_related_name = 'comments'
+
+    def __str__(self):
+        return self.text[:MAX_COMMENT_LENGTH]
