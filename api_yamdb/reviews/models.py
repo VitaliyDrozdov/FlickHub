0ad35efe6b5658c1307195852dbcf66d1c3d201e
greg2707 <mdsk95@gmail.com>
2024-02-15 19:23:28 +0300

    Setting ordering for titles,genres,categories models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -41,6 +41,7 @@ class Title(models.Model):
     class Meta:
         verbose_name = 'Произведение'
         verbose_name_plural = 'Произведения'
+        ordering = ('id',)
 
     def __str__(self):
         return self.name
