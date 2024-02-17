69481727a945bde6756041262b254935a6cd1335
Vitaliy Drozdov <vitdrzdv@mail.ru>
2024-02-17 15:38:28 +0300

    delete imports in views

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, mixins, permissions, viewsets
-from rest_framework.pagination import PageNumberPagination
+from rest_framework import filters, mixins, viewsets
+
 
 from api.serializers import (
     CategorySerializer,
